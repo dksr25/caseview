753002deaa1fbb40e76f4ce17f006901e1d3393e
dksr25 <dksr25@naver.com>
2019-11-21 11:29:13 +0900

    add v-1.0.1

---
+++ 1.0.1/README.md
@@ -0,0 +1,49 @@
+## caseview
+
+###  1.dom구조 세팅
+
+~~~
+<div id="caseview" class="caseview"></div>
+~~~
+
+다음과 같이 body가 닫히는 바로 위에 dom구조를 먼저 잡아주세요
+
+### 2.script 호출
+
+~~~
+<div id="caseview" class="caseview"></div>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://dksr25.github.io/caseview/js/caseview.js"></script>
+~~~
+
+잡아놓은 dom구조 하단에 jquery와 caseview 스크립트를 호출해 주세요
+
+
+### 3.옵션값 입력
+
+~~~
+<script>
+$(document).ready(function(){
+  $('#caseview').caseOpen({
+    zIndex: "3000",
+    position: "bottom-right",
+    caseLength: "6",
+    case1: "bg-blue?$(body).css({'background-color':'#00f'}).find('h2').text('blue')",
+    case2: "bg-yellow?$(body).css({'background-color':'#0f0'}).find('h2').text('green')",
+    case3: "bg-green?$(body).css({'background-color':'#f90'}).find('h2').text('yellow')",
+    case4: "bg-#CCC?$(body).css({'background-color':'#ccc'}).find('h2').text('#CCC')",
+    case5: "bg-red?$(body).css({'background-color':'#f00'}).find('h2').text('red')",
+    case6: "bg-violet?$(body).css({'background-color':'#f0f'}).find('h2').text('violet')"
+  })
+});  
+</script>
+~~~
+
+마지막으로 옵션값 설정 입니다. case 갯수만큼 'caseLength' 값을 부여해 주세요.
+케이스 갯수에 맞게 caseN: " 케이스텍스트 '?' 클릭시 동작구문 " 값을 설정해 주세요. 
+
+zIndex 는 #caseview 에 style로 z-index 값을 부여합니다. 상황에 맞게 부여해주세요. 기본값은 1000 입니다. 
+
+position 은 케이스뷰 모듈의 위치를 결정합니다. 'top-right' , 'top-left' , 'bottom-right' , 'bottom-left' 총 4가지 이며 기본값은 'bottom-left' 입니다.
+
+또한 모바일 환경에서는 드래그하여 드래그가 끝나는 지점에서 가장 가까운 위치로 재위치 됩니다. 
---
+++ 1.0.1/js/caseview.js
@@ -0,0 +1,118 @@
+var objLink = document.createElement("link"); 
+objLink.rel = "stylesheet"; 
+objLink.type = "text/css"; 
+objLink.href = "https://dksr25.github.io/caseview/css/style.css";
+// objLink.href = "css/style.css"; 
+document.head.appendChild(objLink);
+
+(function ( $ ) {
+  $.fn.caseOpen = function( options ) {
+      settings = $.extend({
+      zIndex: "1000",
+      position: "bottom-left"
+    }, options );
+    maxHeight = window.innerHeight - 220;
+    $('#caseview').css({"z-index":settings.zIndex}).addClass(settings.position);
+    $('#caseview').append('<ul class="caseview_lst" /><button type="button" class="caseview_toggle" />');
+    for (var i=0;i<settings.case.length;i++){
+      var str = '<li><button type="button"><span class="txt"><span /></span></button></li>';
+      $('.caseview_lst').append(str);
+      $('.caseview_lst > li').eq(i).find('button').attr('onClick',settings.case[i][1]).find('.txt span').text(settings.case[i][0]); 
+    }
+    // console.log(settings.case[0]);
+    $('.caseview_lst').find('button').on('click',function(){
+      $(this).addClass('on').parent().siblings().find('button').removeClass('on');
+    });
+    $('.caseview_toggle').click(function(){
+      caseToggle($(this));
+      $('.caseview').css({'opacity':'1'});
+    });
+    $('#caseview').siblings().on('touchstart',function(){
+      $('.caseview').css({'opacity':'0.2'});
+      if($('.caseview').hasClass('open')){
+        caseClose();
+        $('.caseview').addClass('ready');
+      }
+    });
+    $('#caseview').siblings().on('touchend',function(){
+      $('.caseview').css({'opacity':'1'});
+      if($('.caseview').css('opacity') == '0.2') {
+        $('.caseview').css({'opacity':'1'});
+      }
+      if($('.caseview').hasClass('ready')){
+        caseDeploy();;
+        $('.caseview').removeClass('ready');
+      }
+    });
+    
+    function caseToggle(aa) {
+      $('.caseview_lst li').css({"transition":"all 0.5s"});
+      if(aa.parent().hasClass('open')){
+        caseClose();
+      }
+      else {
+        caseDeploy();
+      }
+    }
+    function caseClose () {
+      $('.caseview_toggle').parent().removeClass('open');
+      $('.caseview_lst').css({"max-height":maxHeight,"overflow-y":"hidden"});
+      $('.caseview_lst').removeClass('scrolling');
+      $('.caseview_lst').scrollTop(0);
+    }
+    function caseDeploy () {
+      $('.caseview_toggle').parent().addClass('open');
+      $('.caseview_lst').css({"max-height":maxHeight,"overflow-y":"auto"});
+      setTimeout(function(){
+        overflowHeight = $('.caseview_lst').prop('scrollHeight') - 10;
+        scrollTest(maxHeight, overflowHeight);
+      },700)
+    }
+    $(window).resize(function(){
+      maxHeight = window.innerHeight - 220;
+      if($('#caseview').hasClass('open')) {
+        $('.caseview_lst').css({"max-height":maxHeight,"overflow-y":"auto"});
+        setTimeout(function(){
+          overflowHeight = $('.caseview_lst').prop('scrollHeight') - 10;
+          scrollTest(maxHeight, overflowHeight);
+        },700)
+      }
+    });
+    $('.caseview_lst').scroll(function(){
+      innerST = $(this).scrollTop();
+      if(innerST > 0) {
+        $('.caseview_lst').removeClass('scrolling');
+      }
+      else {
+        if(!$(this).parent().hasClass('open')){
+          $('.caseview_lst').removeClass('scrolling');
+        }
+        else {
+          $('.caseview_lst').addClass('scrolling');
+        }
+      }
+    });
+    return this;
+    function scrollTest(mH, oH){
+      if(mH < oH) {
+        $('.caseview_lst').addClass('scrolling');
+      }
+      else {
+        $('.caseview_lst').removeClass('scrolling');
+      }
+    }
+  };
+}( jQuery ));
+window.onload = function(){
+  setTimeout(function(){
+    for (var n=0;n<settings.case.length;n++){
+      var textLength = $('.caseview_lst li:nth-child('+n+')').find('.txt span').prop('scrollWidth');
+      var over = 132 - textLength;
+      var overDu = -(Math.round(over/28)-1);
+      // var overDelay = (2 / overDu) * 100;
+      if(textLength > 128) {
+        $('.caseview_lst li:nth-child('+n+')').find('.txt span').addClass('long').css({'--overflow':over,'--overduration':overDu+'s'});
+      }
+    }
+  },200)
+}
